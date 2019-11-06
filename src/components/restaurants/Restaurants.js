66b94ff35eb8aa8b413c6bda9dad54c9f46a4f4d
Alan Krajina <alankrajina88@gmail.com>
2019-11-06 12:47:22 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -6,7 +6,7 @@ class Restaurants extends Component {
    allRestaurants = () => {
     return this.props.restaurants.map( rest => <Restaurant key={rest.id} restaurant={rest} deleteRest={this.props.deleteRest}/>)
    } // restaurant={rest} to mi je omogucilo u restaurant.js { restaurant }
-  
+
 
 
   render() {
